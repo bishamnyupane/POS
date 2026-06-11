1631600a9d50221f0e78102bcf090c2f7d61ee85
Bisham Neupane <88824945+bishamnyupane@users.noreply.github.com>
2026-06-11 16:01:58 +0545

    Update vite.config.js

--- SaaS_Frontend/vite.config.js
+++ SaaS_Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://pos-1-lk1i.onrender.com',
         changeOrigin: true,
       },
     },
